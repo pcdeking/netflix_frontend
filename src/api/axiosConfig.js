0371becda96b56a42d14e8b2e0b3b4ba8adabe8b
paschal chukwuka <pcakadelast@gmail.com>
2026-04-05 17:07:01 +0300

    added backend connection

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://98.93.149.63:8080 ',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    baseURL:'https://98.93.149.63:8080 '
+    
 });
